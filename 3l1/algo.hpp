213a64db5bfaab33b644feddbb149709a66b88b3
andrey <a.i.maslov199907@bk.ru>
2020-04-22 20:13:38 +0300

    добавлена демка 3й лабы

---
+++ 3l1/algo.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include <QSurfaceDataArray>
+
+using namespace QtDataVisualization;
+
+QSurfaceDataArray *getData() {
+    QSurfaceDataArray *data = new QSurfaceDataArray;
+    QSurfaceDataRow *dataRow1 = new QSurfaceDataRow;
+    QSurfaceDataRow *dataRow2 = new QSurfaceDataRow;
+    *dataRow1 << QVector3D(0.0f, 0.1f, 0.5f) << QVector3D(1.0f, 0.5f, 0.5f);
+    *dataRow2 << QVector3D(0.0f, 1.8f, 1.0f) << QVector3D(1.0f, 1.2f, 1.0f);
+    *data << dataRow1 << dataRow2;
+
+    return data;
+}
---
+++ 3l1/mainwindow.cpp
@@ -0,0 +1,20 @@
+#include "mainwindow.h"
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include "algo.hpp"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent): QMainWindow(parent), ui(new Ui::MainWindow) {
+    ui->setupUi(this);
+
+    QSurface3DSeries *series = new QSurface3DSeries;
+    series->dataProxy()->resetArray(getData());
+    surface.addSeries(series);
+
+    ui->horizontalLayout->addWidget(QWidget::createWindowContainer(&surface));
+    surface.show();
+}
+
+MainWindow::~MainWindow() {
+    delete ui;
+}
